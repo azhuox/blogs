615f550f370d23c39042748a25ff6cb2c54e725b
achuo <achuo@vendasta.com>
2019-02-21 12:56:27 -0600

    example of golang semantic import versioning

--- golang/package_versioning/blog.md
+++
@@ -1,9 +0,0 @@
-
-
-
-
-Reference:
-
-https://akomljen.com/kubernetes-api-resources-which-group-and-version-to-use/
-https://github.com/golang-standards/project-layout
-
---
+++ golang/semantic_import_versioning/example/libzoo/v2/client.go
@@ -0,0 +1,67 @@
+package libfoo
+
+// clientImpl - An implementation of Client interface
+//
+type clientImpl struct {
+
+}
+
+// NewClient - return an `Client` interface
+//
+func NewClient() Client {
+	return &clientImpl{}
+}
+
+// Method1 - the implementation of `Method1` method defined in the `Client` interface
+//
+func (c *clientImpl) Method1() error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
+
+// Method2 - the implementation of `Method2` method defined in the `Client` interface
+//
+func (c *clientImpl) Method2() error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
+
+// Method3 - the implementation of `Method3` method defined in the `Client` interface
+//
+func (c *clientImpl) Method3() error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
+
+// Method4 - the implementation of `Method4` method defined in the `Client` interface
+//
+func (c *clientImpl) Method4() error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
+
+// Method5 - the implementation of `Method5` method defined in the `Client` interface
+//
+func (c *clientImpl) Method5(param1 int, param2 string) error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
+
+// Method6 - the implementation of `Method6` method defined in the `Client` interface
+//
+func (c *clientImpl) Method6() error {
+
+	// Concrete implementation details are omitted
+
+	return nil
+}
